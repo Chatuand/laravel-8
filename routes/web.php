b5515f7d485d3233bb077236aa843d9e5d7476b0
Gabriel Alvarez <gabrielalvarez_20@hotmail.com>
2022-11-16 12:59:21 -0400

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\PageController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [PageController::class, 'home'])->name('home');
+Route::get('curso/{course:slug}', [PageController::class, 'course'])->name('course');
+
+Route::middleware([
+  'auth:sanctum',
+  config('jetstream.auth_session'),
+  'verified',
+])->group(function () {
+  Route::get('/dashboard', function () {
+    return view('dashboard');
+  })->name('dashboard');
+});
